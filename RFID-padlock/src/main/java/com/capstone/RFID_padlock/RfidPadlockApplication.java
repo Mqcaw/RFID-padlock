c5e45b7368f8f23e1790cff92b2a5dff88b7c334
Mqcaw <jacks079funk@gmail.com>
2025-02-13 10:11:28 -0500

    Initial Commit

---
+++ RFID-padlock/src/main/java/com/capstone/RFID_padlock/RfidPadlockApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.RFID_padlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfidPadlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfidPadlockApplication.class, args);
+	}
+
+}
---
+++ RFID-padlock/src/test/java/com/capstone/RFID_padlock/RfidPadlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.RFID_padlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfidPadlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
