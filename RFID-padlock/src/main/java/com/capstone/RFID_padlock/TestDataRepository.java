de5f3267e5b027736f5addd8f9db7eead13f785d
Mqcaw <jacks079funk@gmail.com>
2025-02-13 10:28:21 -0500

    Test setup

---
+++ RFID-padlock/src/main/java/com/capstone/RFID_padlock/TestDataRepository.java
@@ -0,0 +1,7 @@
+package com.capstone.RFID_padlock;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestDataRepository extends JpaRepository<TestData, Long> {
+
+}
