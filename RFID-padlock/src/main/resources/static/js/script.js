ae975302f23a027e12626be0dbefb5eb09eafa33
Jackson Funk <71405306+Mqcaw@users.noreply.github.com>
2025-03-18 08:59:52 -0400

    Update script.js indentation and comments.

--- RFID-padlock/src/main/resources/static/js/script.js
+++ RFID-padlock/src/main/resources/static/js/script.js
@@ -1,53 +1,57 @@
 let sortDirection = {};
 
 function sortTable(colIndex) {
-const table = document.querySelector("table tbody");
-const rows = Array.from(table.rows);
-const arrow = document.getElementById(`arrow-${colIndex}`);
+    const table = document.querySelector("table tbody");
+    const rows = Array.from(table.rows);
+    const arrow = document.getElementById(`arrow-${colIndex}`);
 
-sortDirection[colIndex] = !sortDirection[colIndex];
-rows.sort((rowA, rowB) => {
-let cellA = rowA.cells[colIndex].innerText.toLowerCase();
-let cellB = rowB.cells[colIndex].innerText.toLowerCase();
+    sortDirection[colIndex] = !sortDirection[colIndex];
+    rows.sort((rowA, rowB) => {
+        let cellA = rowA.cells[colIndex].innerText.toLowerCase();
+        let cellB = rowB.cells[colIndex].innerText.toLowerCase();
 
-if (!isNaN(cellA) && !isNaN(cellB)) {
-cellA = parseFloat(cellA);
-cellB = parseFloat(cellB);
+        if (!isNaN(cellA) && !isNaN(cellB)) {
+            cellA = parseFloat(cellA);
+            cellB = parseFloat(cellB);
+        }
+        return sortDirection[colIndex] ? cellA.localeCompare(cellB, undefined, {numeric: true}) : cellB.localeCompare(cellA, undefined, {numeric: true});
+    });
+
+    table.append(...rows);
+
+
+    document.querySelectorAll(".arrow").forEach(arrow => arrow.innerText = "🞁");
+    arrow.innerText = sortDirection[colIndex] ? "🞁" : "🞃";
 }
 
-return sortDirection[colIndex] ? cellA.localeCompare(cellB, undefined, {numeric: true}) : cellB.localeCompare(cellA, undefined, {numeric: true});
-
-
-});
-
-table.append(...rows);
-
-
-document.querySelectorAll(".arrow").forEach(arrow => arrow.innerText = "🞁");
-arrow.innerText = sortDirection[colIndex] ? "🞁" : "🞃";
+//TODO: rename function
+//show key card modal function for onclick
+//should only be called by the keycard modal on student page.
+function assignKeyCard() {
+    document.getElementById('keyCardModal').style.display = 'block';
 }
 
-    function assignKeyCard() {
-        document.getElementById('keyCardModal').style.display = 'block';
-    }
+//close key card modal function for onclick
+//should only be called by the keycard modal on student page.
+function closeKeyCardModal() {
+    document.getElementById('keyCardModal').style.display = 'none';
+}
 
-    function closeKeyCardModal() {
-        document.getElementById('keyCardModal').style.display = 'none';
-    }
-
-    function updateStudent() {
+//function for update student form pn student page.
+//gets the form data and sends makes api call to update the student
+function updateStudent() {
     const formData = new FormData(document.getElementById('studentForm'));
 
-    // Create an object from the form data
+    //map objects from the form data
     const data = {};
     formData.forEach((value, key) => {
         data[key] = value;
     });
 
-    // Log the data to check if the ID is included
-    console.log(data); // Debugging line to check the content of data object
+    
+    console.log(data); //debugging line to check the contents of data
 
-    // Send a PUT request using fetch
+    // Send a PUT request 
     fetch('/api/students/' + data.id, {
         method: 'PUT',
         headers: {
